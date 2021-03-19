032dade04d75162566412458c72ea2cbb2a82eae
Ali ABDALLA <aabdalla@linagora.com>
2021-03-19 16:21:40 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import Footer from './component/Footer';
+import Header from './component/Header'
+import TodosDetail from './views/Todos/TodosDetail';
+import React, {Component} from 'react';
+import {Route,BrowserRouter, Switch} from 'react-router-dom';
+import TodosList from './views/Todos/TodosList';
+import UserList from './views/User/UserList';
+import UserDetail from './views/User/UserDetail';
+import TodosAdd from './views/Todos/TodosAdd';
+import TodosUpdate from './views/Todos/TodosUpdate';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends Component{
+  render(){
+    return <BrowserRouter>
+      <Header />
+      <Switch>
+        <Route exact path="/liste" component={TodosList} />
+        
+        <Route exact path="/liste/:id" component={TodosDetail} />
+        <Route exact path="/liste/:id/ajouter" component={TodosAdd} />
+        <Route exact path="/liste/:id/modifier" component={TodosUpdate} />
+        <Route exact path="/utilisateur" component={UserList} />
+        <Route exact path="/utilisateur/:id" component={UserDetail} />
+
+
+      </Switch>
+
+      
+      <Footer/>
+    </BrowserRouter>
+  }
 }
 
-export default App;
